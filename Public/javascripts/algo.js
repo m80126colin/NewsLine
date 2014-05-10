4db7cc93c3f232afd678bd7f91820fcaa469363b
m80126colin <m80126colin@gmail.com>
2014-05-10 15:04:58 +0800

    Now without event

--- Public/javascripts/algo.js
+++ Public/javascripts/algo.js
@@ -37,6 +37,7 @@ var MyDate = function(msec) {
 	// time.year_maxday
 	time.year_maxday = (time.isleap)? 366: 365;
 };
+var UNIX = new MyDate(0);
 
 // =================================================================
 //
@@ -83,10 +84,10 @@ var Result = function(n, t, sl) {
 	var res = this;
 	/* **************************************************** */
 	/*														*/
-	/*	title												*/
+	/*	topic												*/
 	/*														*/
 	/* **************************************************** */
-	res.title	= t;
+	res.topic	= t;
 
 	/* **************************************************** */
 	/*														*/
@@ -104,13 +105,13 @@ var Result = function(n, t, sl) {
 	/*	tag													*/
 	/*														*/
 	/* **************************************************** */
-	res.tag		= [];
+	var tmpTag	= [];
 	var revTag	= {};
 	for (var i = 0; i < res.news.length; i++) {
 		var tmpTags = res.news[i].tag;
 		for (var j = 0; j < tmpTags.length; j++) {
 			if (revTag[tmpTags[j]] == undefined) {
-				res.tag.push(tmpTags[j]);
+				tmpTag.push(tmpTags[j]);
 				revTag[tmpTags[j]] = 0;
 			}
 			revTag[tmpTags[j]]++;
@@ -119,7 +120,12 @@ var Result = function(n, t, sl) {
 	var tagCount = function(tagA, tagB) {
 		return -(revTag[tagA] - revTag[tagB]);
 	}
-	res.tag.sort(tagCount);
+	tmpTag.sort(tagCount);
+	var tagSelectAlgo = function(tag) {
+		// todo
+		return tag;
+	}
+	res.Tag = tagSelectAlgo(tmpTag);
 
 	/* **************************************************** */
 	/*														*/
@@ -129,41 +135,168 @@ var Result = function(n, t, sl) {
 	var unit = new Unit();
 	var time_slice = [
 		// (special) 15 min
-		function () { return unit.min() * 15; },
+		function (msec) {
+			var slice	= unit.min() * 15;
+			return Math.floor(msec / slice);
+		},
 		// 1 hr
-		function () { return unit.hr(); },
+		function (msec) {
+			var slice	= unit.hr();
+			return Math.floor(msec / slice);
+		},
 		// 6 hrs
-		function () { return unit.hr() * 6; },
+		function (msec) {
+			var slice	= unit.hr() * 6;
+			return Math.floor(msec / slice);
+		},
 		// 1 day
-		function () { return unit.day(); },
+		function (msec) {
+			var slice	= unit.day();
+			return Math.floor(msec / slice);
+		},
 		// 1 week
-		function () { return unit.week(); },
+		function (msec) {
+			var slice	= unit.week();
+			msec += UNIX.week * unit.day();
+			return Math.floor(msec / slice);
+		},
 		// 1 month
-		function (msec) { return unit.month(msec); },
+		function (msec) {
+			var date = new MyDate(msec);
+			return (date.year - UNIX.year) * 12 + (date.month - 1);
+		},
 		// 1 season
-		function (msec) { return unit.season(msec); },
+		function (msec) {
+			var date = new MyDate(msec);
+			return (date.year - UNIX.year) * 4 + Math.floor((date.month - 1) / 3);
+		},
 		// 1 year
-		function (msec) { return unit.year(msec); },
+		function (msec) {
+			return date.year - UNIX.year;
+		},
 		// (special) 2 years
-		function (msec) { return unit.year(msec) * 2; },
+		function (msec) {
+			return Math.floor((date.year - UNIX.year) / 2);
+		}
 	];
+	var rev_time_slice = [
+		// (special) 15 min
+		function (id) {
+			var slice	= unit.min() * 15;
+			return slice * id;
+		},
+		// 1 hr
+		function (id) {
+			var slice	= unit.hr();
+			return slice * id;
+		},
+		// 6 hrs
+		function (id) {
+			var slice	= unit.hr() * 6;
+			return slice * id;
+		},
+		// 1 day
+		function (id) {
+			var slice	= unit.day();
+			return slice * id;
+		},
+		// 1 week
+		function (id) {
+			// todo
+			var slice	= unit.week();
+			return slice * id - UNIX.week * unit.day();
+		},
+		// 1 month
+		function (id) {
+			var y = Math.floor(id / 12) + UNIX.year,
+				m = id % 12;
+			return Date.UTC(y, m);
+		},
+		// 1 season
+		function (id) {
+			var y = Math.floor(id / 4) + UNIX.year,
+				m = id % 4;
+			return Date.UTC(y, m);
+		},
+		// 1 year
+		function (id) {
+			var y = id + UNIX.year;
+			return Date.UTC(y, 0);
+		},
+		// (special) 2 years
+		function (id) {
+			var y = id * 2 + UNIX.year;
+			return Date.UTC(y, 0);
+		}
+	];
+	// setup maximum number of slice
 	var slice_limit = 10;
 	if (sl) slice_limit = sl;
 	/*
 		try possible way
 	*/
-	var count = function(news, time_func) {
-		var offset = 0;
-		if (time_func(0) == unit.week()) // test week
-			offset = 3 * unit.day();
+	var count = function(news, slice_func) {
+		var info = {};
+		info.array	= [];
+		info.dist	= {};
+		// hash every news in timeline block
+		for (var i = 0; i < news.length; i++) {
+			var id = slice_func(news[i].time);
+			if (info.dist[id] == undefined) {
+				info.dist[id] = [];
+				info.array.push(id);
+			}
+			info.dist[id].push(news[i]);
+		}
+		// sort them with time nondecreasingly
+		info.array.sort();
+		for (var id in info.dist) {
+			info.dist[id].sort(newsTime);
+		}
+		// count length
+		var tmp = [];
+		for (var i = 0; i < info.array.length; i++) {
+			tmp.push(info.array[i]);
+			if (i + 1 < info.array.length &&
+				info.array[i + 1] - info.array[i] > 1)
+				tmp.push(-1);
+		}
+		info.array = tmp;
+		info.length = info.array.length;
+		return info;
 	}
-	for (var level = 1; level < time_slice.length; level++) {
+	var level = 0;
+	for (level = 1; level < time_slice.length; level++) {
 		var block_info = count(res.news, time_slice[i]);
 		if (block_info.length <= slice_limit)
-			break;
+			break ;
 	}
 	if (level == time_slice.length)
 		throw 'Error: no match time slice!';
-	//
+	// process block_info into block
+	res.block = [];
+	for (var i = 0; i < block_info.length; i++) {
+		var b = {
+			'isdelimeter': false,
+			'isopen': false,
+			'start': 0,
+			'end': 0,
+			'level': level,
+			'news': [],
+			'event': []
+		};
+		// block with news
+		var id = block_info.array[i];
+		if (id >= 0) {
+			b.start	= rev_time_slice[level](id);
+			b.end	= rev_time_slice[level](id);
+			b.news	= block_info.dist[id];
+		}
+		// vacant
+		else {
+			b.isdelimeter = true;
+		}
+		res.block.push(b);
+	}
 }
 var result = new Result(news, tag);
