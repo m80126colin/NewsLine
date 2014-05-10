424d17fd07ed04d3f9c0eff6005cd5012f10243c
kluns <kluns1206@gmail.com>
2014-05-10 16:37:26 +0800

    scrawler for apple

---
+++ Controllers/curl.js
@@ -0,0 +1,16 @@
+var http = require("http");
+
+// Utility function that downloads a URL and invokes
+// callback with the data.
+
+exports.download=function(url, callback) {
+	http.get(url, function(res) {
+		var data = "";
+		res.on('data', function(chunk) {data += chunk;});
+		res.on("end", function() {callback(data);});
+	})
+	.on("error", function() {callback(null);});
+}
+
+
+
