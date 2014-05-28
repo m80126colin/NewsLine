95ff22d472c1d9fb35542aef6ed9f7b6d8ab3d8c
m80126colin <m80126colin@gmail.com>
2014-05-29 04:38:28 +0800

    Add some function

---
+++ Public/js/temp.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+	// process timeline format
+	var result = {
+		'timeline': {
+			'headline':	'腥聞',
+			'type':		'default',
+			'text':		'身體',
+			'asset': {
+				'media': '',
+				'credit': '',
+				'caption': ''
+			},
+			'date': [],
+			'era': []
+		}
+	};
+	$.getJSON('data.json', function (data) {
+		for (var i = 0; i < data.length; i++) {
+			var time		= new Date(data[i].date);
+			var time_str	= [
+				time.getFullYear(),
+				time.getMonth() + 1,
+				time.getDate()
+				].join(',');
+			result.date.push({
+				'startDate':	time_str,
+				'endDate':		time_str,
+				'headline':		data[i].title,
+				'text':			'haha',
+				'asset': {
+					'media':		data[i].img,
+					'thumbnail':	data[i].img,
+					'credit':		'',
+					'caption':		data[i].id
+				}
+			});
+		}
+		createStoryJS({
+			type:		'timeline',
+			width:		'100%',
+			height:		'80%',
+			lang:		'zh-tw',
+			source:		result,
+			embed_id:	'timeline'
+		});
+	});
+});
