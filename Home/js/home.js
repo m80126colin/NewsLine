5cb5c8d73eb5d7cdf51f778f30c4e480a85dd018
kluns <kluns1206@gmail.com>
2014-06-03 22:47:51 +0800

    home.js

--- Home/js/home.js
+++ Home/js/home.js
@@ -7,7 +7,15 @@ $(function(){
 	var canvas=d3.select('#bubble').append('svg')
 				.attr('width',width)
 				.attr('height',height)
-				
+	
+	var tooltip = d3.select("body")
+				.append("div")
+				.attr('class','fuck')
+				.style("position", "absolute")
+				.style("z-index", "10")
+				.style("visibility", "hidden")
+				.text("a simple tooltip");
+	
 				
 	var pack=d3.layout.pack()
 				.sort(null)
@@ -41,16 +49,27 @@ $(function(){
 			.attr("opacity",0.5)
 			.attr("stroke", function(d){ return d.children? "#fff" : "#ADADAD"; })
 			.attr("stroke-width","1")
-			.on("click", function(d){d.children? console.log('base') : window.open("http://google.com.tw","_self");});
-		
+			.style('cursor','pointer')
+			.on("click", function(d){return d.children? console.log('base') : window.open("http://google.com.tw","_self");});
+			//.on("mouseover", function(){return tooltip.style("visibility", "visible");})
+			//.on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+			//.on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+			
+			
 		node.append("text")
 			.attr("dy", ".3em")
 			.style("text-anchor", "middle")
+			.style('cursor','pointer')
 			.style('font', function(d){ var str=d.className+"";var len=6-(str.length);return (d.r*len/6)+"px sans-serif" ;})
 			.text(function(d){ return d.children ? "" : d.className; })
 			.on("click", function(d){d.children? console.log('base') : window.open("http://google.com.tw","_self");});
 		
+		
 	});
 	
-		
+	var backcircleDelete = setTimeout(function(){$('g:first').remove();},100);
+	$('.node').hover(function(){alert('in')},function(){alert('out')});
+	
+	
+	
 });
