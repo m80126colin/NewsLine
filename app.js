6fa11d37932d78323c281ff8295fbe2e97e1f50d
Shiany <min@Shih-mintekiMacBook-Air.local>
2014-06-05 05:45:38 +0800

    mix_re-4.6

--- app.js
+++ app.js
@@ -6,12 +6,12 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var flash = require('connect-flash');
-var mongoose = require('mongoose');
-var passport = require('passport');
+//var flash = require('connect-flash');
+//var mongoose = require('mongoose');
+//var passport = require('passport');
 
-var public_dir	= __dirname + '/Public';
-var package_dir	= __dirname + '/node_modules';
+//var public_dir	= __dirname + '/Public';
+//var package_dir	= __dirname + '/node_modules';
 var controllers	= require('./Controllers');
 var app = express();
 
@@ -28,18 +28,18 @@ app.use(express.methodOverride());
 //app.use(express.session());
 
 // https://github.com/jaredhanson/passport#middleware
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 // Session based flash messages
-app.use(flash());
+//app.use(flash());
 
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 // public source
-app.use('/public/bootstrap', express.static(public_dir + '/bootstrap'));
-app.use('/public/d3', express.static(path.join(package_dir + '/d3')));
-app.use('/public/vis', express.static(path.join(package_dir + '/vis/dist')));
-app.use('/public/custom', express.static(path.join(public_dir + '/custom')));
+//app.use('/public/bootstrap', express.static(public_dir + '/bootstrap'));
+//app.use('/public/d3', express.static(path.join(package_dir + '/d3')));
+//app.use('/public/vis', express.static(path.join(package_dir + '/vis/dist')));
+//app.use('/public/custom', express.static(path.join(public_dir + '/custom')));
 
 // development only
 if ('development' == app.get('env')) {
