88e3309e9b82d9d1f0407ac9d564d8e94f8ebbe3
Shiany <min@Shih-mintekiMacBook-Air.local>
2014-06-05 02:31:22 +0800

    ver1

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 
-var controllers	= require('./Controllers');
+require('./config/db'); // TODO [DB] : Connect to database
+require('./config/passport'); // TODO [FB] : Passport configuration
 var port		= 5000;
 
 var app = express();
@@ -25,10 +26,10 @@ app.use(express.cookieParser(process.env.COOKIE_SECRET));
 app.use(express.session());
 
 // https://github.com/jaredhanson/passport#middleware
-//app.use(passport.initialize());
-//app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 // Session based flash messages
-//app.use(flash());
+app.use(flash());
 
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -38,11 +39,10 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-//app.get('/', function(req, res){
-//  var messages = req.flash('info');
-//  res.render('index', {messages: messages});
-//});
-app.get('/', controllers.renderIndex);
+app.get('/', function(req, res){
+  var messages = req.flash('info');
+  res.render('index', {messages: messages});
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
